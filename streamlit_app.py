ba89066b08bc1a1abdb564675026339a0621d5cb
Frankie Eder <frankaeder@gmail.com>
2022-12-25 00:41:13 -0800

    Added motivation comment for HEIC functionality.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from PIL import Image
 from pyzbar.pyzbar import decode
 from pillow_heif import register_heif_opener
 
-register_heif_opener()
+register_heif_opener()  # Needed to open HEIC files, common on new iPhones
 
 st.title("Handy Barcode Scanner")
 st.write("Upload an image below to scan barcodes, "
