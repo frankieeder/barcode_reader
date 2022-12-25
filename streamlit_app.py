3f4414f3a96f7de6f2e039435340cbcc3a9ece55
Frankie Eder <frankaeder@gmail.com>
2022-12-24 23:47:37 -0800

    Forgot to update with image.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from PIL import Image
 from pyzbar.pyzbar import decode
 
 image = st.file_uploader("Upload a photo...")
-barcodes = decode(Image.open('pyzbar/tests/code128.png'))
+barcodes = decode(Image.open(image))
 st.write(barcodes)
 
 for barcode in barcodes:
